7d5d33b5075080131a4d78c9344d2044c8e65da9
kulsoom <kulsoom.mujtaba@gmail.com>
2023-07-28 15:28:42 +0500

    Amount and tip input fields and button added to ui

--- app/src/main/java/com/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/tipcalculator/MainActivity.kt
@@ -3,13 +3,28 @@ package com.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +32,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TipCalculatorTheme {
+                var amount by remember {
+                    mutableStateOf("")
+                }
+                var tip by remember {
+                    mutableStateOf("")
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement= Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()) {
+                        InputField(amount,
+                            onValueChange = { input-> amount = input },
+                            placeholderText = "Enter an Amount")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        InputField(tip,
+                            onValueChange = { input-> tip = input },
+                            placeholderText = "Enter tip percentage")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {}) {
+                            Text(text = "Calculate Tip")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun InputField(value: String,
+                       onValueChange: (String) -> Unit,
+                       placeholderText: String, modifier: Modifier = Modifier) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(text = placeholderText) },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number
+        ),
+        modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TipCalculatorTheme {
-        Greeting("Android")
     }
 }
